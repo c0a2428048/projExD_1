8d7bc717417c431206f7c2a4134c19b601fd5e4f
C0A24280 <c0a2428048@edu.teu.ac.jp>
2025-06-10 15:42:17 +0900

    背景延長

--- pygame_image.py
+++ pygame_image.py
@@ -17,7 +17,8 @@ def main():
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
-        screen.blit(bg_img, [tmr, 0])
+        screen.blit(bg_img, [-tmr, 0])
+        screen.blit(bg_img, [-tmr+1600, 0])
         screen.blit(kk_img, [300, 200])
         pg.display.update()
         tmr += 1        

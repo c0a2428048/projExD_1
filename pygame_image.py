c520d39a8c26f9c3d3accd9c1f9810bd76b95864
C0A24280 <c0a2428048@edu.teu.ac.jp>
2025-06-10 16:01:07 +0900

    こうかとん動作

--- pygame_image.py
+++ pygame_image.py
@@ -13,17 +13,30 @@ def main():
     bg_img2 =pg.transform.flip(bg_img,True,False)
     kk_img = pg.image.load("fig/3.png")
     kk_img = pg.transform.flip(kk_img,True,False)
+    kk_rct = kk_img.get_rect()
+    kk_rct.center = 300,200
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
+
+        key_lst = pg.key.get_pressed()
+        if key_lst[pg.K_UP]:
+            kk_rct.move_ip((0, -1))
+        elif key_lst[pg.K_DOWN]:
+            kk_rct.move_ip((0, 1))
+        elif key_lst[pg.K_LEFT]:
+            kk_rct.move_ip((-1, 0))
+        elif key_lst[pg.K_RIGHT]:
+            kk_rct.move_ip((1, 0))
+        
         if tmr==3200:
             tmr=0
 
         screen.blit(bg_img, [-tmr, 0])
         screen.blit(bg_img2, [-tmr+1600, 0])
         screen.blit(bg_img, [-tmr+3200 ,0])
-        screen.blit(kk_img, [300, 200])
+        screen.blit(kk_img, kk_rct)
         pg.display.update()
         tmr += 1        
         clock.tick(200)
